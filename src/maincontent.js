fd5c896def2d22d6654ef729cf00b1a537df74a1
Nabil <nabilomi1@gmail.com>
2021-07-04 19:06:34 -0400

    align typewriter effect

---
+++ src/maincontent.js
@@ -0,0 +1,82 @@
+import './maincontent.css';
+
+export default function Content() {
+    return (
+        <div className="content">
+            <TypeEff />
+        </div>
+    )
+}
+
+function TypeEff() {
+    return (
+        <div className="init-body">
+            <h1>
+                <a href="" className="typewrite" data-period="2000" data-type='[ "Hello!", "I am Nabil.", "I&apos;m a student at CUNY CCNY." ]'>
+                    <span className="wrap"></span>
+                </a>
+            </h1>
+        </div>
+    )
+}
+
+/*
+    Not my code!
+    Yoinked From: https://css-tricks.com/snippets/css/typewriter-effect/
+*/
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};

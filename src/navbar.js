5b75621a17ddabcb74ee653428c00e07f503e037
Nabil <nabilomi1@gmail.com>
2021-07-04 19:06:45 -0400

    change position to absolute

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,7 @@ export default function SideNavbar() {
 
     return (
         <nav className="side-nav" id="side-nav">
-            <header id="header"><strong>Nabil</strong></header>
+            <header id="header"><strong><a className="header-a" href="#top">Nabil</a></strong></header>
             <ul className="nav-ul">
                 <li className="nav-li"><a className="nav-a" href="#about">About</a></li>
                 <li className="nav-li"><a className="nav-a" href="#projects">Projects</a></li>

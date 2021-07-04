1c74777ad6dfa01fc9d5b19520b06f96ba92908b
Nabil <nabilomi1@gmail.com>
2021-07-04 18:21:23 -0400

    add nav

---
+++ src/navbar.js
@@ -0,0 +1,17 @@
+import './navbar.css';
+
+export default function SideNavbar() {
+
+    return (
+        <nav className="side-nav" id="side-nav">
+            <header id="header"><strong>Nabil</strong></header>
+            <ul className="nav-ul">
+                <li className="nav-li"><a className="nav-a" href="#about">About</a></li>
+                <li className="nav-li"><a className="nav-a" href="#projects">Projects</a></li>
+                <li className="nav-li"><a className="nav-a" href="#resume">Contact</a></li>
+                <li className="nav-li"><a className="nav-a" href="#resume">GitHub</a></li>
+                <li className="nav-li"><a className="nav-a" href="#github">Resume</a></li>
+            </ul>
+        </nav>
+    )
+}

b7d069c9f2e63d62bae7eb837478f9fdf26e1acc
Nabil <nabilomi1@gmail.com>
2021-07-04 19:06:22 -0400

    add typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SideNavbar from './navbar';
+import Content from './maincontent.js';
 
 function App() {
   return (
     <div className="App">
       <SideNavbar />
+      <Content />
     </div>
   );
 }
